cf8ed920e17ceef00d185036de99c068207f4dac
Ni Wang <ni.wang@tno.nl>
2024-02-12 15:30:02 +0100

    Add functions and tests

--- src/TulipaPlots.jl
+++ src/TulipaPlots.jl
@@ -1,5 +1,16 @@
 module TulipaPlots
 
-# Write your package code here.
+# Packages
+
+## Graphs
+using Graphs, MetaGraphsNext
+
+## Optimization
+using HiGHS
+
+## Plots
+using Plots, Colors, GraphMakie, GraphMakie.NetworkLayout, CairoMakie
+
+include("plot.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
-using TulipaPlots
 using Test
+using TulipaEnergyModel
+using Graphs
+using HiGHS
 
-@testset "TulipaPlots.jl" begin
-  # Write your tests here.
+# Folders names
+const INPUT_FOLDER = joinpath(@__DIR__, "inputs")
+const OUTPUT_FOLDER = joinpath(@__DIR__, "outputs")
+
+# Run all files in test folder starting with `test-` and ending with `.jl`
+test_files = filter(file -> startswith("test-")(file) && endswith(".jl")(file), readdir(@__DIR__))
+for file in test_files
+    include(file)
 end
