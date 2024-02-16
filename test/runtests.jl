966e0c9861b58bf9dfb37dccf49de611b7e92fb9
Ni Wang <ni.wang@tno.nl>
2024-02-16 15:03:44 +0100

    Remove redundant Pkg

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using TulipaEnergyModel
 using TulipaPlots
 using Test
-using Pkg
 
 # Run all files in test folder starting with `test-` and ending with `.jl`
 test_files = filter(file -> startswith("test-")(file) && endswith(".jl")(file), readdir(@__DIR__))
